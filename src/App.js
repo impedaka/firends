6767628f265efe95f79c880252211b9d1ef491bf
Alice Yu <75595719+impedaka@users.noreply.github.com>
2021-02-28 18:25:12 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -129,7 +129,7 @@ function ChatMessage(props) {
 
   return (<>
     <div className={`message ${messageClass}`}>
-      <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} />
+      <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} alt="alsfjdka"/>
       <p>{text}</p>
     </div>
   </>)
